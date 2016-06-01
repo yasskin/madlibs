621ba2f973bc819043cefd595b15e3fd8feeba51
Joel Delight and Noah Yasskin <noah@>
2016-06-01 09:28:11 -0700

    update JavaScript adding numerous bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-    $("#blanks form").submit(function(event) {
-      var person1Input = $("input#person1").val();
-      var person2Input = $("input#person2").val();
-      var animalInput = $("input#animal").val();
-      var exclamationInput = $("input#exclamation").val();
-      var verbInput = $("input#verb").val();
-      var nounInput = $("input#noun").val();
+  $("#blanks form").submit(function() {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-      $(".person1").text(person1Input);
-      $(".person2").text(person2Input);
-      $(".animal").text(animalInput);
-      $(".exclamation").text(exclamationInput);
-      $(".verb").text(verbInput);
-      $(".noun").text(nounInput);
-
-      $("#story").show();
-
-      event.preventDefault()
+    blanks.forEach(function(blank() {
+      var userInput = $("input." + blank).val();
+      $("." + blank).text(userInput).val();
     });
+
+    $("#story").sho();
+  });
 });
